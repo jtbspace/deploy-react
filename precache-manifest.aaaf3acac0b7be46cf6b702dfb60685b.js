bda6882bfbf79c2cf6f5002d7e2547d6adf3374e
jtbspace <jtbspace.tech@gmail.comm>
2020-03-22 16:47:05 +1300

    Updates

---
+++ precache-manifest.aaaf3acac0b7be46cf6b702dfb60685b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4d49c6c881aab77d73cc1d82891648",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "23598e9cad5a1496ffc5",
+    "url": "/deploy-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ced672b2e5f6c11ffea",
+    "url": "/deploy-react/static/js/2.ff14a9d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.ff14a9d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23598e9cad5a1496ffc5",
+    "url": "/deploy-react/static/js/main.4bdeca17.chunk.js"
+  },
+  {
+    "revision": "eb6eb4e82bafe64e0817",
+    "url": "/deploy-react/static/js/runtime-main.13f9f27d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
